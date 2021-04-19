299b89a119b4cee1da9dda53ccc014a159ac8f22
uhobeike <tatuhiro315@ezweb.ne.jp>
2021-04-19 19:06:12 +0900

    Fix Comment out

--- scripts/logicool_cmd_vel.py
+++ scripts/logicool_cmd_vel.py
@@ -13,24 +13,22 @@ class JoyTwist(object):
         self._smooth_twist_pub = rospy.Publisher('/raw_cmd_vel', Twist, queue_size=1)
         
         self.smooth_flag = False
-    '''
-        self.level = 1
+        #self.level = 1
     
-    def limitter(self, lvl):
-        if lvl <= 0:
-            return 1
-        if lvl >= 6:
-            return 5
-        return lvl
-    '''
+    #def limitter(self, lvl):
+        #if lvl <= 0:
+            #return 1
+        #if lvl >= 6:
+            #return 5
+        #return lvl
+
     def joy_callback(self, joy_msg):
-        '''
-        if joy_msg.buttons[7] == 1:
-            self.level += 1
-        if joy_msg.buttons[6] == 1:
-            self.level -= 1
-        self.level = self.limitter(self.level)
-        '''
+        #if joy_msg.buttons[7] == 1:
+            #self.level += 1
+        #if joy_msg.buttons[6] == 1:
+            #self.level -= 1
+        #self.level = self.limitter(self.level)
+
         twist = Twist()
         if joy_msg.buttons[0] == 1:
             #twist.linear.x = joy_msg.axes[1] * 0.4 * self.level
@@ -53,10 +51,9 @@ class JoyTwist(object):
                 self._smooth_twist_pub.publish(twist)
             elif not self.smooth_flag:
                 self._twist_pub.publish(twist)
-        '''
-        if joy_msg.axes[1] == joy_msg.axes[0] == 0:
-            self.level -= 1
-        '''
+
+        #if joy_msg.axes[1] == joy_msg.axes[0] == 0:
+            #self.level -= 1
 
 if __name__ == '__main__':
     rospy.wait_for_service('/motor_on')
