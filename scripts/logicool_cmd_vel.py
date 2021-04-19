cff6c2c5b1ebb898b64dd01c1b507596eaaaf792
uhobeike <tatuhiro315@ezweb.ne.jp>
2021-04-19 18:53:20 +0900

    Add smooth_flag & Comment out limiter function

--- scripts/logicool_cmd_vel.py
+++ scripts/logicool_cmd_vel.py
@@ -11,40 +11,52 @@ class JoyTwist(object):
         self._joy_sub = rospy.Subscriber('/joy', Joy, self.joy_callback, queue_size=1)
         self._twist_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
         self._smooth_twist_pub = rospy.Publisher('/raw_cmd_vel', Twist, queue_size=1)
-
+        
+        self.smooth_flag = False
+    '''
         self.level = 1
-
+    
     def limitter(self, lvl):
         if lvl <= 0:
             return 1
         if lvl >= 6:
             return 5
         return lvl
-
+    '''
     def joy_callback(self, joy_msg):
+        '''
         if joy_msg.buttons[7] == 1:
             self.level += 1
         if joy_msg.buttons[6] == 1:
             self.level -= 1
         self.level = self.limitter(self.level)
-
+        '''
         twist = Twist()
         if joy_msg.buttons[0] == 1:
-            twist.linear.x = joy_msg.axes[1] * 0.4 * self.level
-            twist.angular.z = joy_msg.axes[0] * 3.14 / 32 * (self.level + 15)
+            #twist.linear.x = joy_msg.axes[1] * 0.4 * self.level
+            #twist.angular.z = joy_msg.axes[0] * 3.14 / 32 * (self.level + 15)
+            twist.linear.x = joy_msg.axes[1] * 0.4
+            twist.angular.z = joy_msg.axes[0] * 3.14 / 32 * 15
             self._twist_pub.publish(twist)
+            self.smooth_flag = False
         elif joy_msg.buttons[2] == 1:
-            twist.linear.x = joy_msg.axes[1] * 0.4 * self.level
-            twist.angular.z = joy_msg.axes[0] * 3.14 / 32 * (self.level + 15)
+            #twist.linear.x = joy_msg.axes[1] * 0.4 * self.level
+            #twist.angular.z = joy_msg.axes[0] * 3.14 / 32 * (self.level + 15)
+            twist.linear.x = joy_msg.axes[1] * 0.4
+            twist.angular.z = joy_msg.axes[0] * 3.14 / 32 * 15
             self._smooth_twist_pub.publish(twist)
+            self.smooth_flag = True;
         else:
             twist.linear.x = 0
             twist.angular.z = 0
-            self._twist_pub.publish(twist)
-
+            if self.smooth_flag:
+                self._smooth_twist_pub.publish(twist)
+            elif not self.smooth_flag:
+                self._twist_pub.publish(twist)
+        '''
         if joy_msg.axes[1] == joy_msg.axes[0] == 0:
             self.level -= 1
-
+        '''
 
 if __name__ == '__main__':
     rospy.wait_for_service('/motor_on')
